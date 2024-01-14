31a9c1ce02b100c254cf222a40a893ba7fe31896
Timur <timk01@mail.ru>
2024-01-14 14:32:04 +0300

    3. Итератор четных чисел [#150]

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (data.length == 0) {
+            return false;
+        }
+        while (index < data.length - 1 && data[index] % 2 != 0) {
+            index++;
+        }
+        return index < data.length && data[index] % 2 == 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+}
---
+++ src/test/java/ru/job4j/iterator/EvenNumbersIteratorTest.java
@@ -0,0 +1,91 @@
+package ru.job4j.iterator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
+
+class EvenNumbersIteratorTest {
+    private Iterator<Integer> iterator;
+
+    @BeforeEach
+    public void setUp() {
+        iterator = new EvenNumbersIterator(new int[] {1, -3, 2, 3, 5, 5, -4, 5, 6, 7, -11, 13});
+    }
+
+    @Test
+    void shouldReturnEvenNumbersSequentially() {
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(2);
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(-4);
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(6);
+        assertThat(iterator.hasNext()).isFalse();
+        assertThatThrownBy(iterator::next)
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(2);
+        assertThat(iterator.next()).isEqualTo(-4);
+        assertThat(iterator.next()).isEqualTo(6);
+    }
+
+    @Test
+    void sequentialNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(iterator.next()).isEqualTo(2);
+        assertThat(iterator.next()).isEqualTo(-4);
+        assertThat(iterator.next()).isEqualTo(6);
+        assertThatThrownBy(iterator::next)
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void sequentialHasNextInvocationWorksFine() {
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+    }
+
+    @Test
+    void  shouldReturnFalseIfNoAnyEvenNumbers() {
+        iterator = new EvenNumbersIterator(new int[]{1});
+        assertThat(iterator.hasNext()).isFalse();
+    }
+
+    @Test
+    void  shouldReturnFalseIfNoAnyNumbers() {
+        iterator = new EvenNumbersIterator(new int[]{});
+        assertThat(iterator.hasNext()).isFalse();
+    }
+
+    @Test
+    void allNumbersAreEven() {
+        iterator = new EvenNumbersIterator(new int[] {2, 4, 6, 8});
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(2);
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(4);
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(6);
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.next()).isEqualTo(8);
+        assertThat(iterator.hasNext()).isFalse();
+    }
+
+    @Test
+    void allNumbersAreOdd() {
+        iterator = new EvenNumbersIterator(new int[] {1, 3, 5, 7});
+        assertThat(iterator.hasNext()).isFalse();
+        assertThat(iterator.hasNext()).isFalse();
+    }
+}
