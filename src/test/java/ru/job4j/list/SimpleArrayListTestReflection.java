1d7ce02fdc76437b5bc64629d3b362701bc99406
Timur <timk01@mail.ru>
2024-01-19 09:15:07 +0300

    + reflection test for future reference

---
+++ src/test/java/ru/job4j/list/SimpleArrayListTestReflection.java
@@ -0,0 +1,53 @@
+package ru.job4j.list;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SimpleArrayListTestReflection {
+
+    private SimpleList<Integer> list;
+
+    @BeforeEach
+    void initData() {
+        list = new SimpleArrayList<>(3);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    /**
+     * for future reference and usage:
+     * test passes, but gives and exception below
+     *
+     * java.lang.ClassCastException: class [Ljava.lang.Object; cannot be cast to class [Ljava.lang.Integer;
+     * ([Ljava.lang.Object; and [Ljava.lang.Integer; are in module java.base of loader 'bootstrap')
+     *
+     * container = (T[]) new Object[capacity]; the problem is here.
+     */
+
+    @Disabled
+    @Test
+    void checkContainerIncrease() {
+        try {
+            list = new SimpleArrayList<>(0);
+            list.add(1);
+            Field privateField
+                    = SimpleArrayList.class.getDeclaredField("container");
+            privateField.setAccessible(true);
+            Object o = privateField.get(list);
+            assertThat(((Integer[]) (o)).length).isEqualTo(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
