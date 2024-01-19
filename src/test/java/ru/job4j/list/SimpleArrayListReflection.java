e92174e1e9071ed8503e700f49895f7b9bf1e6f4
Timur <timk01@mail.ru>
2024-01-19 12:43:27 +0300

    refl changed class

--- src/test/java/ru/job4j/list/SimpleArrayListReflection.java
+++ src/test/java/ru/job4j/list/SimpleArrayListReflection.java
@@ -5,10 +5,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
