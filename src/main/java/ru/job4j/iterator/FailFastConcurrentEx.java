e3353abe80629b222b11e829d284af200fea2724
Timur <timk01@mail.ru>
2024-01-21 12:14:55 +0300

    7. ListIterator [#350217 #475312]

--- src/main/java/ru/job4j/iterator/FailFastConcurrentEx.java
+++ src/main/java/ru/job4j/iterator/FailFastConcurrentEx.java
@@ -10,7 +10,7 @@ public class FailFastConcurrentEx {
          * works fine.
          */
 
-        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext(); ) {
+        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
             Integer next = iterator.next();
             if (next.equals(5)) {
                 iterator.remove();
@@ -28,14 +28,30 @@ public class FailFastConcurrentEx {
         }
         System.out.println(list);
 
+        /**
+         * fails! use listIterator
+         */
+
+        /**
         List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
         ListIterator<Integer> iterator2 = list2.listIterator();
         while (iterator2.hasNext()) {
             Integer value = iterator2.next();
-            if (value <= 5) {
+            if (value <= 3) {
                 list2.add(value * value);
             }
             iterator2.next();
         }
+         */
+
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ListIterator<Integer> iterator3 = list3.listIterator();
+        while (iterator3.hasNext()) {
+            Integer value = iterator3.next();
+            if (value <= 3) {
+                iterator3.add(value * value);
+            }
+        }
+        list3.forEach(System.out::println);
     }
 }
