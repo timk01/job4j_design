ad8e9b4d9e4041c799df61c3599746cd239cc631
Timur <timk01@mail.ru>
2024-01-30 22:07:07 +0300

    8. Тестирование IO [#173905]

---
+++ src/main/java/ru/job4j/io/Abuse.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Abuse {
+
+    public static void drop(String source, String target, List<String> words) throws IOException {
+        try (BufferedReader input = new BufferedReader(new FileReader(source));
+             PrintWriter output = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            Stream<String> stringStream = input.lines()
+                    .flatMap(line -> Stream.of(line.split("\\s+")));
+                    /*.filter(word -> !words.contains(word))
+                    .map(word -> word + " ")
+                    .forEach(output::print);*/
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Path path
+                = Paths.get("C:\\Users\\Тимур");
+        File source = path.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)) {
+            output.println("hello foolish dude");
+            output.println("java job4j php");
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AnalysisTest {
+
+    @Test
+    void unavailable(@TempDir Path tempDir) throws IOException {
+        File sourceFile = tempDir.resolve("server.log").toFile();
+        try (PrintWriter printWriter =
+                     new PrintWriter(
+                             new BufferedOutputStream(
+                                     new FileOutputStream(sourceFile)))) {
+            printWriter.println("200 10:56:01");
+            printWriter.println("500 10:57:01");
+            printWriter.println("400 10:58:01");
+            printWriter.println("500 10:59:01");
+            printWriter.println("400 11:01:02");
+            printWriter.println("300 11:02:02");
+        }
+
+        File targetFile = tempDir.resolve("target.csv").toFile();
+        new Analysis().unavailable(sourceFile.getAbsolutePath(), targetFile.getAbsolutePath());
+
+        List<String> list = new ArrayList<>();
+        try (BufferedReader bufferedReader =
+                     new BufferedReader(new FileReader(targetFile))) {
+            bufferedReader.lines()
+                    .forEach(list::add);
+        }
+
+        assertThat(list).hasSize(1);
+        assertThat(list.get(0)).isEqualTo("10:57:01;11:02:02");
+    }
+}
