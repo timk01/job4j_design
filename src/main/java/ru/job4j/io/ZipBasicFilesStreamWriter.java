5bcf6a35a5091a05bb2cef44ab51044da109a92e
Timur <timk01@mail.ru>
2024-02-05 14:18:58 +0300

    Refactored other way for write files (ZIP) - stream API, - basic writer via buffer

--- src/main/java/ru/job4j/io/ZipBasicFilesStreamWriter.java
+++ src/main/java/ru/job4j/io/ZipBasicFilesStreamWriter.java
@@ -9,17 +9,17 @@ import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public class ZipBasicFilesWriter {
+public class ZipBasicFilesStreamWriter {
 
     private static final int BUFFER_SIZE = 4096;
 
     /**
-     * the one read with classic predetermined bufferSize and loop
+     * the one to read with classic predetermined bufferSize and loop
      * @param sources
      * @param target
      */
 
-    public void packFiles(List<Path> sources, File target) {
+    public void packFilesViaBuffer(List<Path> sources, File target) {
         try (ZipOutputStream zip =
                      new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path path : sources) {
@@ -38,6 +38,32 @@ public class ZipBasicFilesWriter {
         }
     }
 
+    /**
+     * the one to read/write with streams API
+     * @param sources
+     * @param target
+     */
+
+    public void packFilesViaStream(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            sources.forEach(path -> {
+                try {
+                    zip.putNextEntry(new ZipEntry(path.toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try (BufferedInputStream output =
+                             new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                    zip.write(output.readAllBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getName()));
@@ -50,7 +76,7 @@ public class ZipBasicFilesWriter {
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        ZipBasicFilesWriter zip = new ZipBasicFilesWriter();
+        ZipBasicFilesStreamWriter zip = new ZipBasicFilesStreamWriter();
         ArgsName threeParams = ArgsName.of(
                 new String[]{args[0], args[1], args[2]}
         );
@@ -62,6 +88,6 @@ public class ZipBasicFilesWriter {
         Predicate<Path> excludePredicateExt = path ->
                 !path.getFileName().toFile().toString().endsWith(excludeExtension);
         List<Path> list = Search.search(start, excludePredicateExt);
-        zip.packFiles(list, new File(nameOfZippedProject));
+        zip.packFilesViaStream(list, new File(nameOfZippedProject));
     }
 }
