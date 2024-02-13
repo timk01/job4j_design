974d3b72f48f3f52d75e948a834e0ad2675341bc
Timur <timk01@mail.ru>
2024-02-13 22:51:50 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,26 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Timur K.";
+        int age = 38;
+        char sex = 'M';
+        boolean jobless = true;
+        boolean single = true;
+        float tasksAvg = 2.19F;
+        byte daysStudy = (byte) 182;
+        long tries = 100_500_000_000_000L;
+        double successTaskRate = 0.356789;
+        LOG.debug("User info "
+                + "name: {}, age: {}, sex: {}, jobless: {}, single: {},"
+                       + " tasksAvg: {}, daysStudy: {}, tries: {}, successTaskRate: {}",
+                name,
+                age,
+                sex,
+                jobless,
+                single,
+                tasksAvg,
+                daysStudy,
+                tries,
+                successTaskRate);
     }
 }
