e8a9b06cc077ad502cec7af65411b757d3973e31
Timur <timk01@mail.ru>
2024-02-21 11:16:19 +0300

    reworked Analysis (from IO block) added one test here

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -2,6 +2,19 @@ package ru.job4j.io;
 
 import java.io.*;
 
+/**
+ * it was like this (and honestly, it's more readable);
+ * additionally it can be done via chars like char == '4' since responses are grouped by code at the ebginning
+ *
+ * if (status && (line.startsWith("400") || line.startsWith("500"))) {
+ *                     printWriter.printf("%s%s", split[1], ";");
+ *                     status = false;
+ *                 } else if (!status && (line.startsWith("200") || line.startsWith("300"))) {
+ *                     printWriter.printf("%s%s", split[1], System.lineSeparator());
+ *                     status = true;
+ *                 }
+ **/
+
 public class Analysis {
     public void unavailable(String source, String target) {
         boolean status = true;
@@ -12,14 +25,14 @@ public class Analysis {
                              new BufferedOutputStream(
                                      new FileOutputStream(target)))) {
             String line;
+            String separator;
+
             while ((line = bufferedReader.readLine()) != null) {
-                String[] split = line.split(" ");
-                if (status && (line.startsWith("400") || line.startsWith("500"))) {
-                    printWriter.printf("%s%s", split[1], ";");
-                    status = false;
-                } else if (!status && (line.startsWith("200") || line.startsWith("300"))) {
-                    printWriter.printf("%s%s", split[1], System.lineSeparator());
-                    status = true;
+                separator = status ? ";" : System.lineSeparator();
+                if ((status && (line.startsWith("400") || line.startsWith("500"))
+                        || (!status && (line.startsWith("200") || line.startsWith("300"))))) {
+                    printWriter.printf("%s%s",  line.split(" ")[1], separator);
+                    status = !status;
                 }
             }
         } catch (IOException e) {
--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -41,4 +41,33 @@ class AnalysisTest {
         assertThat(list.get(0)).isEqualTo("10:57:01;11:02:02");
     }
 
+    @Test
+    void unavailable2(@TempDir Path tempDir) throws IOException {
+        File sourceFile = tempDir.resolve("server.log").toFile();
+        try (PrintWriter printWriter =
+                     new PrintWriter(
+                             new BufferedOutputStream(
+                                     new FileOutputStream(sourceFile)))) {
+            printWriter.println("200 10:56:01");
+            printWriter.println("500 10:57:01");
+            printWriter.println("400 10:58:01");
+            printWriter.println("300 10:59:01");
+            printWriter.println("500 11:01:02");
+            printWriter.println("200 11:02:02");
+        }
+
+        File targetFile = tempDir.resolve("target.csv").toFile();
+        new Analysis().unavailable(sourceFile.getAbsolutePath(), targetFile.getAbsolutePath());
+
+        List<String> list = new ArrayList<>();
+        try (BufferedReader bufferedReader =
+                     new BufferedReader(new FileReader(targetFile))) {
+            bufferedReader.lines()
+                    .forEach(list::add);
+        }
+
+        assertThat(list).hasSize(2);
+        assertThat(list.get(0)).isEqualTo("10:57:01;10:59:01");
+        assertThat(list.get(1)).isEqualTo("11:01:02;11:02:02");
+    }
 }
