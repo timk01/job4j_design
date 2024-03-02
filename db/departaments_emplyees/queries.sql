0fee1b7587b49e3473d071eaff88bff36631b3b6
Timur <timk01@mail.ru>
2024-03-02 09:06:58 +0300

    0. Join [#6862 #484075]

--- db/departaments_emplyees/queries.sql
+++ db/departaments_emplyees/queries.sql
@@ -63,7 +63,7 @@ select t1.name as "first name",
        t2.name as "second name"
 from teens t1
          cross join teens t2
-where t1.gender != t2.gender and t1.name < t2.name;
+where t1.gender < t2.gender;
 /*
 Ava,Liam
 Ava,Noah
