fe4deb508dfce863dfbf69cbcc5147c356ebe834
Timur <timk01@mail.ru>
2024-03-24 18:17:26 +0300

    Debug in JDBC added

--- src/main/java/ru/job4j/jdbc/City.java
+++ src/main/java/ru/job4j/jdbc/City.java
@@ -1,12 +1,17 @@
 package ru.job4j.jdbc;
 
+import java.util.StringJoiner;
+
 public class City {
 
     private int id;
-
     private String name;
-
     private int population;
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
 
     public City(int id, String name, int population) {
         this.id = id;
@@ -36,5 +41,14 @@ public class City {
 
     public void setPopulation(int population) {
         this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", City.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("population=" + population)
+                .toString();
     }
 }
