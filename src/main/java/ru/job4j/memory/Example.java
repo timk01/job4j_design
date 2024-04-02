d7f8dea2aa6f3b65dc461a581b847b2e0ab81454
Timur <timk01@mail.ru>
2024-04-02 23:33:13 +0300

    GC folder. Stack, heap, String example and pass-by-value

---
+++ src/main/java/ru/job4j/memory/Example.java
@@ -0,0 +1,43 @@
+package ru.job4j.memory;
+
+import java.util.StringJoiner;
+
+public class Example {
+    public static void main(String[] args) {
+        int x = 0;
+        String str = "abc";
+        A a = new A(15);
+        System.out.println(x);
+        System.out.println(a);
+        System.out.println(str);
+        Example example = new Example();
+        example.action(a, x, str);
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(str);
+    }
+
+    public void action(A parameter, int x, String str) {
+        parameter.setIntVal(20);
+        x = 20;
+        str = "bcd";
+    }
+}
+
+class A {
+    int intVal = 10;
+    A(int intVal) {
+        this.intVal = intVal;
+    }
+
+    public void setIntVal(int intVal) {
+        this.intVal = intVal;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", A.class.getSimpleName() + "[", "]")
+                .add("intVal=" + intVal)
+                .toString();
+    }
+}
