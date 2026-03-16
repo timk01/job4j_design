c2c110ed0c604d71960a1364258ebf005df59ba8
Timur <timk01@mail.ru>
2026-03-16 17:33:48 +0300

    0. Принцип открытости закрытости [#4914 #560277]

--- src/main/java/ru/job4j/ood/ocp/homework/example2/CreditCardPaymentProcessorWrongExImpl.java
+++ src/main/java/ru/job4j/ood/ocp/homework/example2/CreditCardPaymentProcessorWrongExImpl.java
@@ -2,7 +2,7 @@ package ru.job4j.ood.ocp.homework.example2;
 
 public class CreditCardPaymentProcessorWrongExImpl extends PaymentProcessorWrongEx {
     public void payWithCreditCard() {
-        super.pay();
+        super.payByDefault();
         System.out.println("and with creditCard too");
     }
 }
