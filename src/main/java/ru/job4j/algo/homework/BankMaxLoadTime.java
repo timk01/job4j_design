d654cfd981e6c262cd64d88989d79b2c3ebc5aa4
Timur <timk01@mail.ru>
2026-05-16 11:07:45 +0300

    Revert "3.2. Откат изменений [#504830]"
    This reverts commit 1a00a3f446da49e2f8c8c33329b07414215ec728.

--- src/main/java/ru/job4j/algo/homework/BankMaxLoadTime.java
+++ src/main/java/ru/job4j/algo/homework/BankMaxLoadTime.java
@@ -55,6 +55,7 @@ public class BankMaxLoadTime {
         int maxClientsQuantity = 0;
         int maxLoadStartTime = 0;
         int maxLoadEndTime = 0;
+
         for (int i = 0; i < events.size() - 1; i++) {
             if (events.get(i).type == ARRIVAL) {
                 currentClientsQuantity++;
